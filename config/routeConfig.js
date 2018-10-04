8738415ce3f21bd7a2b71e720b8439ecdf392bb2
Nick Giammanco <ngiammanco@gmail.com>
2018-10-03 20:35:07 -0400

    Project setup

---
+++ config/routeConfig.js
@@ -0,0 +1,9 @@
+import MainScreen from '../screens/MainScreen';
+import LoginScreen from '../screens/auth/LoginScreen';
+
+export default {
+
+  MainScreen: { screen: MainScreen },
+  LoginScreen: { screen: LoginScreen }
+
+};
