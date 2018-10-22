ece0377887b75df1dcd51e3b92307739547cfb99
Nick Giammanco <ngiammanco@gmail.com>
2018-10-21 21:48:30 -0400

    Leagues screen update

--- screens/MyLeaguesScreen.js
+++ screens/MyLeaguesScreen.js
@@ -5,11 +5,11 @@ export default class RecentMatchesScreen extends React.Component {
     return (
       <ScrollView style={{ backgroundColor: '#484f4f'}}>
       <View style={{ marginTop: 20, alignItems:"center" }}>
-        <Text style={{fontWeight: "bold", color: 'white', padding: 10, fontSize: 20}}> League 1 </Text>
-        <View style={{paddingTop:10}} />
-        <Text style={{fontWeight: "bold", color: 'white', padding: 10, fontSize: 20}}> League 2 </Text>
-        <View style={{paddingTop:10}} />
-        <Text style={{fontWeight: "bold", color: 'white', padding: 10, fontSize: 20}}> League 3 </Text>
+          <Text style={{fontWeight: "bold", color: 'white', padding: 10, fontSize: 20}}> League 1 </Text>
+          <View style={{paddingTop:10}} />
+          <Text style={{fontWeight: "bold", color: 'white', padding: 10, fontSize: 20}}> League 2 </Text>
+          <View style={{paddingTop:10}} />
+          <Text style={{fontWeight: "bold", color: 'white', padding: 10, fontSize: 20}}> League 3 </Text>
         </View>
         </ScrollView>
     );
