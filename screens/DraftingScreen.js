11c63856759f4ca29fa05b4756ffa7fd8ff33ca5
Nick Giammanco <ngiammanco@gmail.com>
2018-10-22 21:30:19 -0400

    Create League functionality

--- screens/DraftingScreen.js
+++ screens/DraftingScreen.js
@@ -1,16 +1,28 @@
 import React from 'react';
-import { StyleSheet, Text, View, Button, ScrollView, ListView, TouchableOpacity, Dimensions, Alert } from 'react-native';
+import { StyleSheet, Text, View, Button, ScrollView, ListView, TouchableOpacity, Dimensions, Alert, TextInput } from 'react-native';
 import * as firebase from 'firebase';
 
 var width = Dimensions.get('window').width;
 export default class DraftingScreen extends React.Component {
   state = {
+    leagueName: "",
     players: [],
-    numPlayers: 0
+    numPlayers: 0,
+    teamName: ""
   }
 
 componentDidMount() {
-  this.setState({ numPlayers: 6 })
+  const league = this.props.navigation.state.params.leagueName;
+  this.setState({ leagueName: league });
+  //Get the number of players from the rules of the league in the database
+  var leagueRef = firebase.database().ref('/league/' + league + '/rules');
+  leagueRef.once('value').then(snapshot => {
+    const numPlayers = snapshot.val().num_players;
+    this.setState({ numPlayers: numPlayers })
+  });
+
+  //Get the players from the database (NOTE: Switch to AUDL when given access)
+  //Create a list of random players with the size being the number of players accessed from the league rules
   var playersRef = firebase.database().ref('/players');
   var playersList = [];
   playersRef.once('value').then(snapshot => {
@@ -26,11 +38,15 @@ componentDidMount() {
       indexList.push(playerIndex);
     }
 
+    //Write the new user to the league and the players that were autodrafted
     var currentIndex = 0;
+    const userID = firebase.auth().currentUser.uid;
+    //Delete players that might already exist there
+    //var playerDelRef = firebase.database().ref('/league/' + league + '/users/' + userID + '/players').remove();
     snapshot.forEach(item => {
       if(indexList.indexOf(currentIndex) != -1){
         playersList.push(item.val());
-        this.writeUserData(firebase.auth().currentUser.uid, item.val().id, item.val().Name, item.val().Team);
+        this.writeUserData(this.state.leagueName, userID, item.val().id, item.val().Name, item.val().Team);
       }
       currentIndex++;
     });
@@ -38,16 +54,24 @@ componentDidMount() {
   })
 }
 
-writeUserData(userID, playerID, name, team) {
-  var teamRef = firebase.database().ref('/league/1/users/' + userID + '/players/' + playerID).set({
-    name: name,
-    team: team
-  });
-}
+  writeUserData(leagueName, userID, playerID, name, team) {
+    var playerRef = firebase.database().ref('/league/' + leagueName + '/users/' + userID + '/players/' + playerID).set({
+      name: name,
+      team: team
+    });
+  }
 
-alertItemName = (item) => {
-    Alert.alert('Team', item.Team)
- }
+  alertItemName = (item) => {
+      Alert.alert('Team', item.Team)
+   }
+
+   handleFinish() {
+     var teamRef = firebase.database().ref('/league/' + this.state.leagueName + '/users/' + firebase.auth().currentUser.uid).update({
+       team_name: this.state.teamName
+     });
+     this.props.navigation.navigate('Main');
+   }
+
   render() {
       return (
         <ScrollView style={{ backgroundColor: '#484f4f' }}>
@@ -73,9 +97,16 @@ alertItemName = (item) => {
               }
               </View>
 
+              <Text style={{fontWeight: "bold", color: 'white', padding: 10, fontSize: 20}}>Team Name</Text>
+              <TextInput style={{width: 200, height: 40, borderWidth: 1, backgroundColor: 'white'}}
+                  onChangeText={(text) => { this.setState({teamName: text}) }}
+                  placeholder="Team Name"
+                  autoCorrect={false}
+              />
+
               <TouchableOpacity
                 style={styles.button}
-                onPress={() => this.props.navigation.navigate('Main')}
+                onPress={() => this.handleFinish()}
                 >
                 <Text style={styles.buttonText}> Finish </Text>
               </TouchableOpacity>
