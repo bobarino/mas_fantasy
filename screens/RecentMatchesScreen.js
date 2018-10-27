cf02fd3a9dded8f8af1eb949b0a21adc308b2cd3
Justin Duan <jduan39@gatech.edu>
2018-10-27 12:14:57 -0400

    testing

--- screens/RecentMatchesScreen.js
+++ screens/RecentMatchesScreen.js
@@ -3,11 +3,15 @@ import { StyleSheet, Text, View, Image } from 'react-native';
 export default class RecentMatchesScreen extends React.Component {
   render() {
     return (
-      <Image
-        style={{flex:1, height: undefined, width: undefined}}
-        source={require('../assets/TeamScoreComparison5.jpg')}
-        resizeMode="contain"
-      />
+      <View style={styles.container}>
+      </View>
     );
   }
 }
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#74b9ff'
+  }
+})
